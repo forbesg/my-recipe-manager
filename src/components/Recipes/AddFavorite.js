340adcaa3490473079b6ec29b84b474816468cbb
Forbes Gray <fnbg75@gmail.com>
2017-05-08 10:19:31 +0100

    Centre the icon

--- src/components/Recipes/AddFavorite.js
+++ src/components/Recipes/AddFavorite.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function AddFavorite (props) {
   return (
-    <div className="favorite-container" data-tooltip="Add to Favorites" onClick={props.handleClick}>
+    <div className="favorite-container tooltip" data-tooltip="Add to Favorites" onClick={props.handleClick}>
       <i className="fa fa-heart"></i>
     </div>
   );
