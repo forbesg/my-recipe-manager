00e38f405d279113e99d427ed620dc3f2f35626e
Forbes Gray <fnbg75@gmail.com>
2018-06-16 14:21:38 +0100

    Add runtime cache to sw-precache

--- src/sw-precache-config.js
+++ src/sw-precache-config.js
@@ -5,6 +5,27 @@ module.exports = {
     'build/manifest.json',
     'build/static/**/!(*map*)'
   ],
+  runtimeCaching: [{
+    cacheName: 'resource-cache',
+    urlPattern: /\.(?:html|js|css)$/,
+    handler: 'cacheFirst'
+  }, {
+    // Use a custom cache name.
+    cacheName: 'images',
+
+    // Match any request ends with .png, .jpg, .jpeg or .svg.
+    urlPattern: /\.(?:png|jpg|jpeg|svg)/,
+
+    // Apply a cache-first strategy.
+    handler: 'cacheFirst',
+
+    options: {
+      // Only cache 10 images.
+      expiration: {
+        maxEntries: 20,
+      },
+    },
+  }],
   dontCacheBustUrlsMatching: /\.\w{8}\./,
   swFilePath: 'build/service-worker.js'
 };
